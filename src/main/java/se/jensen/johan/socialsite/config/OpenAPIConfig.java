310d69e1b797c3bd0d19b939e5dc9cc46aafa669
Johan Nihlén <johan.nihlen@hotmail.com>
2026-01-15 15:36:00 +0100

    Uppdaterar OpenAPIConfig

--- src/main/java/se/jensen/johan/socialsite/config/OpenAPIConfig.java
+++ src/main/java/se/jensen/johan/socialsite/config/OpenAPIConfig.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 @OpenAPIDefinition(
         info = @Info(title = "API med JWT", version = "1.0"),
         security = @SecurityRequirement(name = "bearerAuth")
