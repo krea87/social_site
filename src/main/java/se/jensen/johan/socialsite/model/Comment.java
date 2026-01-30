c75b1a7840b4826c61744471842946f9f9a40753
Johan Nihlén <johan.nihlen@hotmail.com>
2026-01-30 22:10:38 +0100

    .

---
+++ src/main/java/se/jensen/johan/socialsite/model/Comment.java
@@ -0,0 +1,34 @@
+package se.jensen.johan.socialsite.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String text;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+
+
+
+
+}
